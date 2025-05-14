c46f35ec426c4e3f323dac9d71c8ba135668a5d0
00039425 <00039425@uca.edu.sv>
2025-05-14 12:29:07 -0600

    (00039425) Finalice el proyecto. Ya esta comentado y todo funciona bien

--- ExamenCorto.cpp
+++ ExamenCorto.cpp
@@ -2,131 +2,272 @@
 
 using namespace std;
 
-int main(){
-int Edad, NivelU, Producto;
-double Saldo = 1000, Precio = 0, Descuento = 0, PrecioFinal = 0, Restante = 0, Faltante = 0;
-
-cout << "Ingrese su edad, por favor: ";
-cin >> Edad;
-
-if(Edad < 16){
-    cout << "Usted no puede continuar con la compra" << endl;
-}
-else{
-    cout << "----Niveles de usuario--- " << endl
-    << "1. Estudiante" << endl
-    << "2. Profesional" << endl
-    << ". Ver todos" << endl;
-
-    cout << "Ingrese su nivel de usuario, por favor: ";
-cin >> NivelU;
-
-switch (NivelU)
+int main()
 {
-case 1:
-    cout << "----Productos para estudiantes--- " << endl
-    << "AVISO: Sera aplicado un 10% de descuento en su compra" << endl
-    << "1. Laptop Básica: $900" << endl
-    << "2. Tablet Estudiantil: $600" << endl
-    << "3. Chromebook: $700" << endl;
+    // Variables a utiliar
+    int Edad, NivelU, Producto;
+    double Saldo = 1000, Precio = 0, Descuento = 0, PrecioFinal = 0, Restante = 0, Faltante = 0;
 
-    cout << "Ingrese el producto que desea comprar: ";
-    cin >> Producto;
+    // se le pide al usuario que ingrese su edad
+    cout << "Ingrese su edad, por favor: ";
+    cin >> Edad;
 
-    switch (Producto)
+    // Si es menor a 16 no puede comprar. Si es mayor puede continuar
+    if (Edad < 16)
     {
-    case 1:
-        Precio = 900;
+        cout << "Usted no puede continuar con la compra" << endl;
+    }
+    else
+    {
+        // Se le pide su nivel de usuario
+        cout << "\n----Niveles de usuario--- " << endl
+             << "1. Estudiante" << endl
+             << "2. Profesional" << endl
+             << "3. Ver todos" << endl;
 
-        Descuento = Precio * 0.20;
+        cout << "Ingrese su nivel de usuario, por favor: ";
+        cin >> NivelU;
 
-        PrecioFinal = Precio - Descuento;
+        // manejo de los distinto casos de niveles de usuario
+        switch (NivelU)
+        {
+        case 1:
+            // Se le muestran los productos de estudiantes
+            cout << "\n----Productos para estudiantes--- " << endl
+                 << "AVISO: Sera aplicado un 20% de descuento en su compra" << endl
+                 << "1. Laptop Básica: $900" << endl
+                 << "2. Tablet Estudiantil: $600" << endl
+                 << "3. Chromebook: $700" << endl;
 
-        
+            cout << "Ingrese el producto que desea comprar: ";
+            cin >> Producto;
 
-        if(PrecioFinal < Saldo){
-            Restante = Saldo - PrecioFinal;
+            switch (Producto)
+            {
+                // Manejo de productos
+            case 1:
+                Precio = 900;
 
-            cout << "Saldo disponible = $1000" << endl
-            << "Commpra exitosa. Usted compro: Laptop Básica" << endl
-            << "Saldo Restante: " << Restante << endl;
-            
-        }
-        else{
-            Faltante = PrecioFinal - Saldo;
+                Descuento = Precio * 0.20;
 
-            cout<< "Saldo disponible: 1000" << endl
-            << "No se pudo realizar la compra. " << endl
-            << "Le faltan $" << Faltante << " para poder proceder"  
-        }
+                PrecioFinal = Precio - Descuento;
 
-        break;
+                // Se valida si el saldo es suficiente o no
+                if (PrecioFinal < Saldo)
+                {
+                    Restante = Saldo - PrecioFinal;
+
+                    cout << "\nSaldo disponible = $1000" << endl
+                         << "descuento: $" << Descuento << endl
+                         << "Commpra exitosa. Usted compro: Laptop Básica" << endl
+                         << "Saldo Restante: " << Restante << endl;
+                }
+                else
+                {
+                    Faltante = PrecioFinal - Saldo;
+
+                    cout << "\nSaldo disponible: 1000" << endl
+                         << "descuento: $" << Descuento << endl
+                         << "No se pudo realizar la compra. " << endl
+                         << "Le faltan $" << Faltante << " para poder proceder";
+                }
+
+                break;
+
+            case 2:
+                Precio = 600;
+
+                Descuento = Precio * 0.20;
+
+                PrecioFinal = Precio - Descuento;
+
+                // Se valida si el saldo es suficiente o no
+                if (PrecioFinal < Saldo)
+                {
+                    Restante = Saldo - PrecioFinal;
+
+                    cout << "\nSaldo disponible = $1000" << endl
+                         << "descuento: $" << Descuento << endl
+                         << "Commpra exitosa. Usted compro: tablet estudiante" << endl
+                         << "Saldo Restante: " << Restante << endl;
+                }
+                else
+                {
+                    Faltante = PrecioFinal - Saldo;
+
+                    cout << "\nSaldo disponible: 1000" << endl
+                         << "descuento: $" << Descuento << endl
+                         << "No se pudo realizar la compra. " << endl
+                         << "Le faltan $" << Faltante << " para poder proceder";
+                }
+
+                break;
+
+            case 3:
+                Precio = 700;
+
+                Descuento = Precio * 0.20;
+
+                PrecioFinal = Precio - Descuento;
+
+                // Se valida si el saldo es suficiente o no
+                if (PrecioFinal < Saldo)
+                {
+                    Restante = Saldo - PrecioFinal;
+
+                    cout << "\nSaldo disponible = $1000" << endl
+                         << "descuento: $" << Descuento << endl
+                         << "Commpra exitosa. Usted compro: Chromebook" << endl
+                         << "Saldo Restante: " << Restante << endl;
+                }
+                else
+                {
+                    Faltante = PrecioFinal - Saldo;
+
+                    cout << "\nSaldo disponible: 1000" << endl
+                         << "descuento: $" << Descuento << endl
+                         << "No se pudo realizar la compra. " << endl
+                         << "Le faltan $" << Faltante << " para poder proceder";
+                }
+
+                break;
+                // si ingresa un nimero fuera del menu se le muestra este mensaje
+            default:
+                cout << "\nIngrese una opcion valida dentro del menu de productos " << endl;
+                break;
+            }
+            break;
 
         case 2:
-        Precio = 600;
+            // se muestran los productos para profesionales
+            cout << "\n----Productos para profesionsales--- " << endl
+                 << "AVISO: Sera aplicado un 10% de descuento en su compra" << endl
+                 << "1. Laptop Avanzada: $1500" << endl
+                 << "2. Tablet Pro: $1200" << endl
+                 << "3. Estación de Trabajo: $2000" << endl;
 
-        Descuento = Precio * 0.20;
+            cout << "Ingrese el producto que desea comprar: ";
+            cin >> Producto;
 
-        PrecioFinal = Precio - Descuento;
+            // Manejo de casos
+            switch (Producto)
+            {
+            case 1:
+                Precio = 1500;
 
-        
+                Descuento = Precio * 0.10;
 
-        if(PrecioFinal < Saldo){
-            Restante = Saldo - PrecioFinal;
+                PrecioFinal = Precio - Descuento;
 
-            cout << "Saldo disponible = $1000" << endl
-            << "Commpra exitosa. Usted compro: tablet estudiante" << endl
-            << "Saldo Restante: " << Restante << endl;
-            
-        }
-        else{
-            Faltante = PrecioFinal - Saldo;
+                // Se valida si el saldo es suficiente o no
+                if (PrecioFinal < Saldo)
+                {
+                    Restante = Saldo - PrecioFinal;
 
-            cout<< "Saldo disponible: 1000" << endl
-            << "No se pudo realizar la compra. " << endl
-            << "Le faltan $" << Faltante << " para poder proceder"  
-        }
+                    cout << "\nSaldo disponible = $1000" << endl
+                         << "descuento: $" << Descuento << endl
+                         << "Commpra exitosa. Usted compro: Laptop Avanzada" << endl
+                         << "Saldo Restante: " << Restante << endl;
+                }
+                else
+                {
+                    Faltante = PrecioFinal - Saldo;
 
-        break;
+                    cout << "\nSaldo disponible: 1000" << endl
+                         << "descuento: $" << Descuento << endl
+                         << "No se pudo realizar la compra. " << endl
+                         << "Le faltan $" << Faltante << " para poder proceder";
+                }
 
+                break;
+
+            case 2:
+                Precio = 1200;
+
+                Descuento = Precio * 0.10;
+
+                PrecioFinal = Precio - Descuento;
+
+                // Se valida si el saldo es suficiente o no
+                if (PrecioFinal < Saldo)
+                {
+                    Restante = Saldo - PrecioFinal;
+
+                    cout << "\nSaldo disponible = $1000" << endl
+                         << "descuento: $" << Descuento << endl
+                         << "Commpra exitosa. Usted compro: tablet pro" << endl
+                         << "Saldo Restante: " << Restante << endl;
+                }
+                else
+                {
+                    Faltante = PrecioFinal - Saldo;
+
+                    cout << "\nSaldo disponible: 1000" << endl
+                         << "descuento: $" << Descuento << endl
+                         << "No se pudo realizar la compra. " << endl
+                         << "Le faltan $" << Faltante << " para poder proceder";
+                }
+
+                break;
+
+            case 3:
+                Precio = 2000;
+
+                Descuento = Precio * 0.10;
+
+                PrecioFinal = Precio - Descuento;
+
+                // Se valida si el saldo es suficiente o no
+                if (PrecioFinal < Saldo)
+                {
+                    Restante = Saldo - PrecioFinal;
+
+                    cout << "\nSaldo disponible = $1000" << endl
+                         << "descuento: $" << Descuento << endl
+                         << "Commpra exitosa. Usted compro: Estacion de trabajo" << endl
+                         << "Saldo Restante: " << Restante << endl;
+                }
+                else
+                {
+                    Faltante = PrecioFinal - Saldo;
+
+                    cout << "\nSaldo disponible: 1000" << endl
+                         << "descuento: $" << Descuento << endl
+                         << "No se pudo realizar la compra. " << endl
+                         << "Le faltan $" << Faltante << " para poder proceder";
+                }
+
+                break;
+                // Si ingresa una opcion fuera del menu se le muestra este mensaje
+            default:
+                cout << "\nIngrese una opcion valida dentro del menu de productos " << endl;
+                break;
+            }
+            break;
+
+            // Eneste caso se le muestran todos los productos dispunibles y sus precios
         case 3:
-        Precio = 700;
+            cout << "A continuacion se le mostraran todos los productos disponibles" << endl;
 
-        Descuento = Precio * 0.20;
+            cout << "\n----Productos para profesionsaless--- " << endl
+                 << "AVISO: Sera aplicado un 10% de descuento en su compra" << endl
+                 << "1. Laptop Avanzada: $1500" << endl
+                 << "2. Tablet Pro: $1200" << endl
+                 << "3. Estación de Trabajo: $2000" << endl;
 
-        PrecioFinal = Precio - Descuento;
+            cout << "\n----Productos para profesionsaless--- " << endl
+                 << "AVISO: Sera aplicado un 10% de descuento en su compra" << endl
+                 << "1. Laptop Avanzada: $1500" << endl
+                 << "2. Tablet Pro: $1200" << endl
+                 << "3. Estación de Trabajo: $2000" << endl;
 
-        
+            // Si ingresa un valor fuera del menu se le muestra este mensaje
+        default:
 
-        if(PrecioFinal < Saldo){
-            Restante = Saldo - PrecioFinal;
-
-            cout << "Saldo disponible = $1000" << endl
-            << "Commpra exitosa. Usted compro: Chromebook" << endl
-            << "Saldo Restante: " << Restante << endl;
-            
+            cout << "\nIngrese una opcion valida dentro del menu de niveles de usuario" << endl;
+            break;
         }
-        else{
-            Faltante = PrecioFinal - Saldo;
-
-            cout<< "Saldo disponible: 1000" << endl
-            << "No se pudo realizar la compra. " << endl
-            << "Le faltan $" << Faltante << " para poder proceder"  
-        }
-
-        break;
-    
-    default:
-    cout << "Ingrese una opcion valida dentro del menu de productos " zz endl;
-        break;
     }
-    break;
-
-default:
-    break;
-}
-}
-
 
     return 0;
 }
